264807b65627c9c195040246e69a30e296bcc756
Your Name <you@example.com>
2022-06-19 19:07:41 -0600

    Updated settings folder with pw and db info + class to models file and migrated to workbench

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Created products class
+
+class Products(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
+    
--- products_project/settings.py
+++ products_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'products_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'products_project',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Noba2i7w@jUg'
     }
 }
 
